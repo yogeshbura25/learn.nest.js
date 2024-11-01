34dd2320569bf43e9072cc227e581ab3eaa59e1a
Yogesh <burayogesh78@gmail.com>
2024-11-01 17:40:38 +0530

    fixed the import issue of prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from './prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
